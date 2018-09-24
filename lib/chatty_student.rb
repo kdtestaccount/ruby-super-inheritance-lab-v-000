3f65b3ebfdefe9d960597fccff2d89a5b58fe65a
kvdoherty <kvdoherty@gmail.com>
2018-09-24 23:58:10 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,13 +5,13 @@ def hello
   puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
 
-# def raise_hand
-#   i = 0
-#   until i == 10
-#     i += 1
-#     puts "Pick me!"
-#   end
-# end
+def raise_hand
+  i = 0
+  until i == 10
+    i += 1
+    puts "Pick me!"
+  end
+end
   
   
 end
